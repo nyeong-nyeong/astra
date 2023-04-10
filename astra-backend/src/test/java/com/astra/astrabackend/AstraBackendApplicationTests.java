915e6f197a5756459216e5ad125aabc7df9d85cd
jong-ny17 <ffeer0508@gmail.com>
2023-04-10 16:22:07 +0900

    first commit

---
+++ astra-backend/src/test/java/com/astra/astrabackend/AstraBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.astra.astrabackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AstraBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
