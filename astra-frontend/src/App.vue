2183f7f7a75e44397ac6320ada4d27ca9dae4921
jong-ny17 <ffeer0508@gmail.com>
2023-04-11 17:59:47 +0900

    vuetify

--- astra-frontend/src/App.vue
+++ astra-frontend/src/App.vue
@@ -1,8 +1,16 @@
 <template>
   <v-app>
+    <v-app-bar app color="primary" dark>
+    <v-app-bar-nav-icon></v-app-bar-nav-icon>
+    <v-tool-bar-title>Astra-jong</v-tool-bar-title>
+    <v-spacer></v-spacer>
+    <v-btn icon><v-icon>mdi-magnify</v-icon></v-btn>
+    </v-app-bar>
     <v-main>
       <HelloWorld/>
     </v-main>
+    <v-footer color="primary">
+    </v-footer>
   </v-app>
 </template>
 
