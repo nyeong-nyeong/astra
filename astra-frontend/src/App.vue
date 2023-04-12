3f6a58a13915e0170faba821d4a3a7d17df5ec4f
jong-ny17 <ffeer0508@gmail.com>
2023-04-12 17:51:00 +0900

    UI 약간

--- astra-frontend/src/App.vue
+++ astra-frontend/src/App.vue
@@ -1,28 +1,31 @@
 <template>
   <v-app>
-    <v-app-bar app color="primary" dark>
-    <v-app-bar-nav-icon></v-app-bar-nav-icon>
-    <v-tool-bar-title>Astra-jong</v-tool-bar-title>
-    <v-spacer></v-spacer>
-    <v-btn icon><v-icon>mdi-magnify</v-icon></v-btn>
+    <v-app-bar app dark>
+    <v-spacer><h2 style="text-align:center">Astra</h2></v-spacer>
     </v-app-bar>
+    <v-if>
     <v-main>
-      <HelloWorld/>
+      <LeftNavBar>
+        <template v-slot:nav>
+          <HelloWorld/>
+        </template>
+    </LeftNavBar>
     </v-main>
-    <v-footer color="primary">
-    </v-footer>
+  </v-if>
   </v-app>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
 import HelloWorld from './components/HelloWorld.vue'
+import LeftNavBar from './components/LeftNavBar.vue'
 
 export default defineComponent({
   name: 'App',
 
   components: {
     HelloWorld,
+    LeftNavBar,
   },
 
   data () {
--- astra-frontend/src/components/HelloWorld.vue
+++ astra-frontend/src/components/HelloWorld.vue
@@ -1,153 +1,50 @@
 <template>
-  <v-container>
-    <v-row class="text-center">
-      <v-col cols="12">
-        <v-img
-          :src="require('../assets/logo.svg')"
-          class="my-3"
-          contain
-          height="200"
-        />
-      </v-col>
-
-      <v-col class="mb-4">
-        <h1 class="display-2 font-weight-bold mb-3">
-          Welcome to Vuetify 3 Beta
-        </h1>
-
-
-        <p class="subheading font-weight-regular">
-          For help and collaboration with other Vuetify developers,
-          <br>please join our online
-          <a
-            href="https://community.vuetifyjs.com"
-            target="_blank"
-          >Discord Community</a>
-        </p>
-      </v-col>
-
-      <v-col
-        class="mb-5"
-        cols="12"
+  <v-container fluid>
+    <v-tabs
+      v-model="tab"
+      color="deep-white-accent-4"
+      align-tabs="center"
+    >
+      <v-tab :value="1">Landscape</v-tab>
+      <v-tab :value="2">City</v-tab>
+      <v-tab :value="3">Abstract</v-tab>
+    </v-tabs>
+  <v-card 
+  max-auto="mx-auto"
+  max-width="100%"
+  style="padding:2% 3% 0% 5%"
+  >
+    <v-window v-model="tab">
+      <v-window-item
+        v-for="n in 3"
+        :key="n"
+        :value="n"
       >
-        <h2 class="headline font-weight-bold mb-5">
-          What's next?
-        </h2>
-
-        <v-row justify="center">
-          <a
-            v-for="(next, i) in whatsNext"
-            :key="i"
-            :href="next.href"
-            class="subheading mx-3"
-            target="_blank"
-          >
-            {{ next.text }}
-          </a>
-        </v-row>
-      </v-col>
-
-      <v-col
-        class="mb-5"
-        cols="12"
-      >
-        <h2 class="headline font-weight-bold mb-5">
-          Important Links
-        </h2>
-
-        <v-row justify="center">
-          <a
-            v-for="(link, i) in importantLinks"
-            :key="i"
-            :href="link.href"
-            class="subheading mx-3"
-            target="_blank"
-          >
-            {{ link.text }}
-          </a>
-        </v-row>
-      </v-col>
-
-      <v-col
-        class="mb-5"
-        cols="12"
-      >
-        <h2 class="headline font-weight-bold mb-5">
-          Ecosystem
-        </h2>
-
-        <v-row justify="center">
-          <a
-            v-for="(eco, i) in ecosystem"
-            :key="i"
-            :href="eco.href"
-            class="subheading mx-3"
-            target="_blank"
-          >
-            {{ eco.text }}
-          </a>
-        </v-row>
-      </v-col>
-    </v-row>
-  </v-container>
+          <v-row dense>
+            <v-col
+              v-for="i in 6"
+              :key="i"
+              cols="12"
+              md="3"
+            >
+              <v-img
+                :src="`https://picsum.photos/500/300?image=${i * n * 5 + 10}`"
+                :lazy-src="`https://picsum.photos/10/6?image=${i * n * 5 + 10}`"
+                aspect-ratio="1"
+              ></v-img>
+            </v-col>
+          </v-row>
+        
+      </v-window-item>
+    </v-window>
+  </v-card>
+</v-container>
 </template>
 
-<script lang='ts'>
-import { defineComponent } from 'vue'
-
-
-export default defineComponent({
-  name: 'HelloWorld',
-
-  data () {
-    return {
-      ecosystem: [
-        {
-          text: 'vuetify-loader',
-          href: 'https://github.com/vuetifyjs/vuetify-loader/tree/next',
-        },
-        {
-          text: 'github',
-          href: 'https://github.com/vuetifyjs/vuetify/tree/next',
-        },
-        {
-          text: 'awesome-vuetify',
-          href: 'https://github.com/vuetifyjs/awesome-vuetify',
-        },
-      ],
-      importantLinks: [
-        {
-          text: 'Chat',
-          href: 'https://community.vuetifyjs.com',
-        },
-        {
-          text: 'Made with Vuetify',
-          href: 'https://madewithvuejs.com/vuetify',
-        },
-        {
-          text: 'Twitter',
-          href: 'https://twitter.com/vuetifyjs',
-        },
-        {
-          text: 'Articles',
-          href: 'https://medium.com/vuetify',
-        },
-      ],
-      whatsNext: [
-        {
-          text: 'Explore components',
-          href: 'https://vuetifyjs.com',
-        },
-        {
-          text: 'Roadmap',
-          href: 'https://vuetifyjs.com/en/introduction/roadmap/',
-        },
-        {
-          text: 'Frequently Asked Questions',
-          href: 'https://vuetifyjs.com/getting-started/frequently-asked-questions',
-        },
-      ],
-    }
-  },
-})
+<script lang="ts">
+  export default {
+    data: () => ({
+      tab: null,
+    }),
+  }
 </script>
